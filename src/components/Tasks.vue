c2df7e8f488e1a859eaa2441335f20253d2a8052
Corwintines <cssmittys@gmail.com>
2021-06-06 22:01:35 -0600

    create basic task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,10 +1,11 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
+import Task from './Task'
 export default {
     name: 'Tasks',
     props: {
@@ -12,5 +13,8 @@ export default {
             type: Array
         }
     },
+    components: {
+        Task
+    }
 }
 </script>
